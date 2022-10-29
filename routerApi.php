17081604776bb36406b04809096584f9e8eb24d1
Pedro Medina <pedrohugomedina@gmail.com>
2022-10-29 09:49:12 -0300

    Incluyo el archivo controllerApiTipoInsumo.php y agrego las rutas para realizar el CRUD en la tabla tipos_insumos

--- routerApi.php
+++ routerApi.php
@@ -1,6 +1,7 @@
 <?php
 require_once './libs/Router.php';
 require_once './app/controllers/controllerApiInsumo.php';
+require_once './app/controllers/controllerApiTipoInsumo.php';
 
 
 // instancio un objeto de la clase Router
@@ -13,6 +14,11 @@ $router->addRoute('insumos/:ID', 'DELETE', 'ApiInsumoController', 'deleteInsumo'
 $router->addRoute('insumos', 'POST', 'ApiInsumoController', 'addInsumo');
 $router->addRoute('insumos/:ID', 'PUT', 'ApiInsumoController', 'updateInsumo');
 
+$router->addRoute('tiposInsumos', 'GET', 'ApiTipoInsumoController', 'getTiposInsumos');
+$router->addRoute('tiposInsumos/:ID', 'GET', 'ApiTipoInsumoController', 'getTipoInsumo');
+$router->addRoute('tiposInsumos/:ID', 'DELETE', 'ApiTipoInsumoController', 'deleteTipoInsumo');
+$router->addRoute('tiposInsumos', 'POST', 'ApiTipoInsumoController', 'addTipoInsumo');
+$router->addRoute('tiposInsumos/:ID', 'PUT', 'ApiTipoInsumoController', 'updateTipoInsumo');
 
 // ejecuta la ruta (sea cual sea)
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
