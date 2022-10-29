5bbb5addbae8e197940535511c904d38de733064
Pedro Medina <pedrohugomedina@gmail.com>
2022-10-29 09:47:55 -0300

    Modifico el modificador de acceso de todas las funciones y en la funcion add agrego un return que me devuelve el ultimo ID ingresado

--- app/models/modelTipoInsumo.php
+++ app/models/modelTipoInsumo.php
@@ -11,7 +11,7 @@ class TipoInsumoModel extends Model {
     /**
      * Consulta para mostrar todos los tipos de insumos
      */
-    function getAll(){
+    public function getAll(){
         $query = $this->db->prepare("SELECT * FROM tipo_insumo");
         $query->execute();
         $tiposInsumos = $query->fetchAll(PDO::FETCH_OBJ);
@@ -21,7 +21,7 @@ class TipoInsumoModel extends Model {
     /**
      * Consulta por ID un determinado tipo de insumo
      */
-    function get($id){
+    public function get($id){
         $query = $this->db->prepare("SELECT id_tipo_insumo, tipo_insumo FROM tipo_insumo WHERE id_tipo_insumo = ?");
         $query->execute([$id]);
         $tipoInsumo = $query->fetch(PDO::FETCH_OBJ);
@@ -31,15 +31,16 @@ class TipoInsumoModel extends Model {
     /**
      * Agrega un nuevo Tipo de Insumo
      */
-    function add($tipo_insumo){
+    public function add($tipo_insumo){
         $query = $this->db->prepare("INSERT INTO tipo_insumo(tipo_insumo) VALUES (?)");
-        $query->execute([$tipo_insumo]);    
+        $query->execute([$tipo_insumo]);
+        return $this->db->lastInsertId();    
     }
 
     /**
      * Elimina un tipo de insumo
      */
-    function delete($id_tipo_insumo){
+    public function delete($id_tipo_insumo){
         $query = $this->db->prepare("DELETE FROM tipo_insumo WHERE id_tipo_insumo = ?");
         $query->execute([$id_tipo_insumo]);   
     }
@@ -47,7 +48,7 @@ class TipoInsumoModel extends Model {
     /**
      * Edita un tipo de insumo
      */
-    function update($id_tipo_insumo, $tipo_insumo){
+    public function update($id_tipo_insumo, $tipo_insumo){
         $query = $this->db->prepare("UPDATE tipo_insumo SET tipo_insumo = ? WHERE id_tipo_insumo = ?");
         $query->execute([$tipo_insumo, $id_tipo_insumo]);   
     }
