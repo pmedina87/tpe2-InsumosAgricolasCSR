6c0083118e20555a7d3e4c93e2e3f9f7e51ca984
Pedro Medina <pedrohugomedina@gmail.com>
2022-11-13 17:20:53 -0300

    Creo archivo para control de autenticacion del usuario

---
+++ app/controllers/controllerApiAuth.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once './app/controllers/controller.php';
+require_once './app/models/modelUser.php';
+require_once './app/models/modelToken.php';
+require_once './app/helpers/helpersApiAuth.php';
+require_once './app/views/viewApi.php';
+
+
+class ApiAuthController extends Controller {
+    
+    private $modelUser;
+    private $authHelpers;
+    
+    public function __construct(){
+        parent::__construct();
+        $this->modelUser = new UserModel();
+        $this->authHelpers = new ApiAuthHelper();
+    }
+
+
+    public function getToken($params = null){
+        //obtengo el header
+        $userAndPass = $this->authHelpers->getBasicHeader();
+        $userHeader = $userAndPass['user'];
+        $passHeader = $userAndPass['pass'];
+
+        $userAndPassDB = $this->modelUser->getByUsername($userHeader);
+        
+        if($userAndPassDB && password_verify($passHeader, $userAndPassDB->contrasenia)){
+            $idDB = $userAndPassDB->id_usuario;
+            $userDB = $userAndPassDB->usuario;
+            $token = $this->authHelpers->createToken($idDB, $userDB);
+            $this->view->response(["Token" => $token], 200);
+        }
+        else {
+            $this->view->response("No autorizado: Usuario y/o contraseña incorrectos", 401);
+        }
+    }
+}
