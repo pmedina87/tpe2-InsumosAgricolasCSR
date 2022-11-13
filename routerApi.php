71bcc9b0f6f813fd9f37f4c1019422cfb436a33e
Pedro Medina <pedrohugomedina@gmail.com>
2022-11-13 17:09:01 -0300

    Agrego ruta getToken

--- routerApi.php
+++ routerApi.php
@@ -2,6 +2,7 @@
 require_once './libs/Router.php';
 require_once './app/controllers/controllerApiInsumo.php';
 require_once './app/controllers/controllerApiTipoInsumo.php';
+require_once './app/controllers/controllerApiAuth.php';
 
 
 // instancio un objeto de la clase Router
@@ -14,12 +15,14 @@ $router->addRoute('insumos/:ID', 'DELETE', 'ApiInsumoController', 'deleteSupplie
 $router->addRoute('insumos', 'POST', 'ApiInsumoController', 'addSupplie');
 $router->addRoute('insumos/:ID', 'PUT', 'ApiInsumoController', 'updateSupplie');
 
+$router->addRoute('auth/token', 'GET', 'ApiAuthController', 'getToken');
 
-$router->addRoute('tiposInsumos', 'GET', 'ApiTipoInsumoController', 'getTiposInsumos');
-$router->addRoute('tiposInsumos/:ID', 'GET', 'ApiTipoInsumoController', 'getTipoInsumo');
-$router->addRoute('tiposInsumos/:ID', 'DELETE', 'ApiTipoInsumoController', 'deleteTipoInsumo');
-$router->addRoute('tiposInsumos', 'POST', 'ApiTipoInsumoController', 'addTipoInsumo');
-$router->addRoute('tiposInsumos/:ID', 'PUT', 'ApiTipoInsumoController', 'updateTipoInsumo');
+
+// $router->addRoute('tiposInsumos', 'GET', 'ApiTipoInsumoController', 'getTiposInsumos');
+// $router->addRoute('tiposInsumos/:ID', 'GET', 'ApiTipoInsumoController', 'getTipoInsumo');
+// $router->addRoute('tiposInsumos/:ID', 'DELETE', 'ApiTipoInsumoController', 'deleteTipoInsumo');
+// $router->addRoute('tiposInsumos', 'POST', 'ApiTipoInsumoController', 'addTipoInsumo');
+// $router->addRoute('tiposInsumos/:ID', 'PUT', 'ApiTipoInsumoController', 'updateTipoInsumo');
 
 // ejecuta la ruta (sea cual sea)
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
